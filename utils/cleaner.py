7eb264f033519b6399a62491e38c418fc623b4be
chili <ymf21971@gmail.com>
2025-08-27 23:01:14 +0300

    finish cleaner

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,7 +1,11 @@
 import string
-import re
-import nltk
+
+#import nltk
 from nltk.corpus import stopwords
+#nltk.download('stopwords')
+from nltk.stem import PorterStemmer
+#nltk.download("punkt")
+
 
 
 class Cleaner:
@@ -9,41 +13,42 @@ class Cleaner:
         self.data = data
         self.clean_data = data
 
-    def removing_punctuation_marks(self):
+    def removing_all_marks(self):
         translator = str.maketrans('', '', string.punctuation)
         clean_text = self.clean_data.translate(translator)
         self.clean_data = clean_text
 
-    def removing_special_marks(self):
-        clean_string = ' '.join(e for e in self.clean_data.split() if e.isalnum())
-        self.clean_data = clean_string
-
-        #self.clean_data = ""
-
-    def removing_long_spacees_end_marks(self):
-        self.clean_data = ""
-    #
-    # def removing_stop_words(self):
-    #     nltk.download('stopwords')
-    #     stop_words = set(stopwords.words('english'))
-
-
-        #print(stopwords.words('english'))
-        # self.clean_data = ""
+    def removing_long_spaces(self):
+        cleaned_text = ' '.join(self.clean_data.split())
+        self.clean_data = cleaned_text
 
     def convert_to_lowercase(self):
-        self.clean_data = ""
+        lowercase_string = self.clean_data.lower()
+        self.clean_data = lowercase_string
+
+    def removing_stop_words(self):
+        stop_words = set(stopwords.words('english'))
+        filtered_sentence = [w for w in self.clean_data.split() if not w in stop_words]
+        self.clean_data = ' '.join(filtered_sentence)
+
 
     def lemtization(self):
-        self.clean_data = ""
+        ps = PorterStemmer()
+        lemtied_data = [ps.stem(word) for word in self.clean_data.split()]
+        self.clean_data = ' '.join(lemtied_data)
+        #self.clean_data = ""
 
 if __name__ == "__main__":
-    cleaner = Cleaner("""This#$ is a sample sentence, showing off the stop words filtration.""")
+    cleaner = Cleaner("""This#$ is a sample/   sentenTCce, @@#@$  showing off   REYE    the stop words filtration.""")
     print(cleaner.clean_data)
-    cleaner.removing_punctuation_marks()
-    #print(cleaner.clean_data)
-    cleaner.removing_special_marks()
-
+    cleaner.removing_all_marks()
     print(cleaner.clean_data)
-
+    cleaner.removing_long_spaces()
+    print(cleaner.clean_data)
+    cleaner.convert_to_lowercase()
+    print(cleaner.clean_data)
+    cleaner.removing_stop_words()
+    print(cleaner.clean_data)
+    cleaner.lemtization()
+    print(cleaner.clean_data)
 
