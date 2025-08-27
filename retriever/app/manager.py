6295f448214d7dacaaad4bd18b82539a56e3ee76
benjyp2003 <benjypfeffer@gmail.com>
2025-08-27 17:40:18 +0300

    Implement manager class for message publishing and classification

---
+++ retriever/app/manager.py
@@ -0,0 +1,24 @@
+from classifier import Classifier
+from dal import Dal
+from kafka_producer import Producer
+
+
+class Manager:
+    def __init__(self):
+        self.producer = Producer()
+        self.dal = Dal("tweets")
+        self.classifier = Classifier()
+
+    def publish_messages(self):
+        try:
+            docs = self.dal.fetch_100_latest_docs()
+            classified_docs = self.classifier.classify_docs(docs)
+            self.producer.publish("raw_tweets_antisemitic", classified_docs.get("antisemitic"))
+            print("Published antisemitic tweets successfully.")
+            self.producer.publish("raw_tweets_not_antisemitic", classified_docs.get("not_antisemitic"))
+            print("Published not antisemitic tweets successfully.")
+
+        except Exception as e:
+            raise Exception(f"Error publishing messages: {e}")
+
+
