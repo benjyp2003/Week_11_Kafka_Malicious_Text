f59706d45f1e0cf4378971214d360bb0a6f7f6ba
benjyp2003 <benjypfeffer@gmail.com>
2025-08-28 16:12:02 +0300

    Refactor weapons data loading and cleaning: modify data format and improve line handling

--- enricher/app/weapon_processing/weapons_cleaning.py
+++ enricher/app/weapon_processing/weapons_cleaning.py
@@ -2,8 +2,8 @@ from enricher.app.weapon_processing.weapons_loader import load_weapons_data
 from utils.cleaner import Cleaner
 
 def get_clean_weapons() -> str:
-    weapons = load_weapons_data("data/weapon_list.txt")
-    cleaner = Cleaner(weapons)
+    weapons = " ".join(load_weapons_data("data/weapon_list.txt"))
 
+    cleaner = Cleaner(weapons)
     cleaner.manager_cleaner()
     return cleaner.clean_data
--- enricher/app/weapon_processing/weapons_loader.py
+++ enricher/app/weapon_processing/weapons_loader.py
@@ -2,7 +2,7 @@ def load_weapons_data(file_path):
     """Load weapons data from a file"""
     try:
         with open(file_path) as f:
-            data = f.read()
+            data = f.read().splitlines()
         return data
     except Exception as e:
         raise Exception(f"An error occurred while loading weapons data: {e}")
